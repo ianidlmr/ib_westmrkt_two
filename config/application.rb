4840e04f4790deb70fb8a7cd609aa283fe3d13ca
WajidKagzi <wjkagzi@gmail.com>
2017-03-06 12:21:56 -0500

    Remove old robots and autoload robots

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,8 @@ module RailwayMrkt
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    # Make sure lib files are auto loaded
+    config.autoload_paths += %W(#{config.root}/lib)
   end
 end
