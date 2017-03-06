9ade9e7fadae1cd0a9afce4f4743212ad9f69096
WajidKagzi <wjkagzi@gmail.com>
2017-03-06 11:07:39 -0500

    Create user model and add devise

---
+++ app/models/user.rb
@@ -0,0 +1,75 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id                     :integer          not null, primary key
+#  email                  :string           default(""), not null
+#  encrypted_password     :string           default(""), not null
+#  reset_password_token   :string
+#  reset_password_sent_at :datetime
+#  remember_created_at    :datetime
+#  sign_in_count          :integer          default("0"), not null
+#  current_sign_in_at     :datetime
+#  last_sign_in_at        :datetime
+#  current_sign_in_ip     :inet
+#  last_sign_in_ip        :inet
+#  created_at             :datetime         not null
+#  updated_at             :datetime         not null
+#
+# Indexes
+#
+#  index_users_on_email                 (email) UNIQUE
+#  index_users_on_reset_password_token  (reset_password_token) UNIQUE
+#
+
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable
+
+  #------------------------------------------------------------------------------
+  # Associations
+
+  #------------------------------------------------------------------------------
+  # Scopes
+
+  #------------------------------------------------------------------------------
+  # Validations
+
+  #------------------------------------------------------------------------------
+  # Callbacks
+  # after_create :create_stripe_account
+  # after_save :update_stripe_account, if: :email_changed?
+
+  #------------------------------------------------------------------------------
+  # Enumerations
+
+  #------------------------------------------------------------------------------
+  # AASM definitions
+
+  #------------------------------------------------------------------------------
+  # Class methods
+
+  #------------------------------------------------------------------------------
+  # Instance methods
+
+  #------------------------------------------------------------------------------
+  # Rails Admin Config
+
+  #------------------------------------------------------------------------------
+  # private
+  # def create_stripe_account
+  #   unless stripe_token.present? || Rails.env.test?
+  #     customer = Stripe::Customer.create(description: "User ID: #{id}", email: email)
+  #     update_columns(stripe_token: customer.id)
+  #   end
+  # end
+
+  # def update_stripe_account
+  #   unless Rails.env.test?
+  #     customer = Stripe::Customer.retrieve(stripe_token)
+  #     customer.email = email
+  #     customer.save
+  #   end
+  # end
+end
