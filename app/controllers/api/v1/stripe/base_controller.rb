2638ee33e0d3a378837d5366bd648bffba62f767
WajidKagzi <wjkagzi@gmail.com>
2017-03-07 10:31:09 -0500

    Add API routes for stripe and connect webhooks

---
+++ app/controllers/api/v1/stripe/base_controller.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+class Api::V1::Stripe::BaseController < ApplicationController
+  skip_before_action :verify_authenticity_token
+  before_action :protect
+
+  # This user/password is added to the webhook URLs in the Stripe settings to ensure we are getting authenticated webhooks directly from Stripe only.
+  # http_basic_authenticate_with(name: Figaro.env.api_http_auth_name, password: Figaro.env.api_http_auth_password) unless Rails.env.production?
+
+  def protect
+    @ips = ['54.187.174.169', '54.187.205.235', '54.187.216.72', '54.241.31.99', '54.241.31.102', '54.241.34.107']
+    unless @ips.include? request.remote_ip
+      render text: 'You are unauthorized'
+      return
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,14 @@ Rails.application.routes.draw do
   devise_for :users
 
   get '/robots.txt' => RobotsTxt # lib/robots_txt.rb
+
+  #------------------------------------------------------------------------------
+  # API ROUTES
+  namespace :api do
+    namespace :v1 do
+      namespace :stripe do
+        resources :account, only: [:create]
+      end
+    end
+  end
 end
