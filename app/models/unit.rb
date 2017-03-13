11f268663ecb631fa1afeaa9777b69bab712f6e3
WajidKagzi <wjkagzi@gmail.com>
2017-03-13 07:51:45 -0400

    Create unit and unit type models

---
+++ app/models/unit.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+# == Schema Information
+#
+# Table name: units
+#
+#  id         :integer          not null, primary key
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
+class Unit < ApplicationRecord
+end
---
+++ app/models/unit_type.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+# == Schema Information
+#
+# Table name: unit_types
+#
+#  id         :integer          not null, primary key
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
+class UnitType < ApplicationRecord
+end
