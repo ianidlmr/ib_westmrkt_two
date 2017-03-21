ae606773ce3ddf2bcc254c939eff5f40a19f9c6e
WajidKagzi <wjkagzi@gmail.com>
2017-03-21 07:49:51 -0400

    Add new assets for layout

---
+++ app/assets/javascripts/common.js
@@ -0,0 +1,45 @@
+$(function() {
+  $('.input-text-wrap input').on('focus', function() {
+    $(this).closest('.input-text-wrap').addClass('is-focused');
+  });
+
+  $('.input-text-wrap input').on('blur', function(e) {
+    if (!$(e.target).val().length) {
+      $(this).closest('.input-text-wrap').removeClass('is-focused');
+    }
+  });
+
+  $('body').on('click', function(e) {
+    // e.preventDefault();
+    if ($(e.target).hasClass('hamburger-icon')) {
+      $('.sidebar').addClass('sidebar-is-open');
+    } else if ($(e.target).hasClass('saved-icon')) {
+      $('.right-sidebar').addClass('right-sidebar-is-open');
+    } else if (!$(e.target).hasClass('sidebar') && !$(e.target).parents('.sidebar').length && $('.sidebar').hasClass('sidebar-is-open')) {
+      $('.sidebar').removeClass('sidebar-is-open');
+    } else if (!$(e.target).hasClass('right-sidebar') && !$(e.target).parents('.right-sidebar').length && $('.right-sidebar').hasClass('right-sidebar-is-open')) {
+      $('.right-sidebar').removeClass('right-sidebar-is-open');
+    }
+  });
+
+
+  $('.simple_form.new_user').validate({
+    rules: {
+      email: {
+        required: true,
+        email: true
+      },
+    },
+
+    highlight: function(element, errorClass, validClass) {
+      $(element).parents('.input-text-wrap').children('.form-control-feedback').hide();
+      $(element).addClass(errorClass).removeClass(validClass);
+      $(element).closest('.input-text-wrap input').removeClass('has-success').addClass('has-error');
+    },
+    success: function(element) {
+      $(element).parents('.input-text-wrap').children('.form-control-feedback').show();
+      $(element).parent().children('input').removeClass('has-error').addClass('has-success');
+      $(element).remove();
+    }
+  });
+});
