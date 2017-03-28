95fd6c48c4e9b65e689fd77092174eda9494a982
WajidKagzi <wjkagzi@gmail.com>
2017-03-28 13:52:20 -0400

    Remove binding pry

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 class SearchController < ApplicationController
   def search
-binding.pry
     @one_bedroom_units = UnitType.where('number_of_bedrooms = ? AND number_of_bathrooms = ? AND den = ? AND balcony = ?', 1, 1, params[:den], params[:balcony]).joins(:units).where('units.price > ?', 100000).map(&:units).flatten
     @two_bedroom_units = UnitType.where('number_of_bedrooms = ? AND number_of_bathrooms = ? AND den = ? AND balcony = ?', 2, 1, params[:den], params[:balcony]).joins(:units).where('units.price > ?', 100000).map(&:units).flatten
     @three_plus_bedroom_units = UnitType.where('number_of_bedrooms >= ? AND number_of_bathrooms = ? AND den = ? AND balcony = ?', 3, 1, params[:den], params[:balcony]).joins(:units).where('units.price > ?', 100000).map(&:units).flatten
