8cab382ee4c71d736967d92396f3eb1fd53aae22
WajidKagzi <wjkagzi@gmail.com>
2017-04-13 09:06:34 -0400

    Categorize liked units

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -4,18 +4,20 @@ class LikesController < ApplicationController
 
   def like_unit
     @likable_unit.likes.create(user: current_user) if @likable_unit.present?
-    @liked_units = current_user.liked_units
+    @liked_unit_groups = current_user&.liked_units.sort_by{ |liked_unit| liked_unit.unit.unit_type.name }.group_by{ |liked_unit| { name: liked_unit.unit.unit_type.name, state: liked_unit.unit.state } }
+    @liked_units_count = current_user&.liked_units.count
   end
 
   def unlike_unit
-    current_user.liked_units.where(unit: @likable_unit).first.destroy if @likable_unit.present?
-    @liked_units = current_user.liked_units
+    current_user.liked_units.where(unit: @likable_unit).first.destroy! if @likable_unit.present?
+    @liked_unit_groups = current_user&.liked_units.sort_by{ |liked_unit| liked_unit.unit.unit_type.name }.group_by{ |liked_unit| { name: liked_unit.unit.unit_type.name, state: liked_unit.unit.state } }
+    @liked_units_count = current_user&.liked_units.count
   end
 
   #------------------------------------------------------------------------------
   private
 
   def find_likable_unit
-    @likable_unit = Unit.find params[:id]
+    @likable_unit = Unit.find(params[:id])
   end
 end
