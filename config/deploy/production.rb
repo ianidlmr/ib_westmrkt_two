ec01ca9c990368fc10ab7806d638f62764702784
WajidKagzi <wjkagzi@gmail.com>
2017-04-27 14:01:36 -0400

    Upload production environment

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -8,17 +8,7 @@
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
-# role-based syntax
-# ==================
-
-# Defines a role with one or multiple servers. The primary server in each
-# group is considered to be the first unless any hosts have the primary
-# property set. Specify the username and a domain or IP for the server.
-# Don't use `:all`, it's a meta role.
-
-# role :app, %w{deploy@example.com}, my_property: :my_value
-# role :web, %w{user1@primary.com user2@additional.com}, other_property: :other_value
-# role :db,  %w{deploy@example.com}
+server '52.60.205.109', user: 'deploy', roles: %w(app db puma_role), primary: true
 
 # Configuration
 # =============
@@ -28,29 +18,24 @@
 # http://capistranorb.com/documentation/getting-started/configuration/
 # Feel free to add new variables to customise your setup.
 
-# Custom SSH Options
-# ==================
-# You may pass any option but keep in mind that net/ssh understands a
-# limited set of options, consult the Net::SSH documentation.
-# http://net-ssh.github.io/net-ssh/classes/Net/SSH.html#method-c-start
-#
-# Global options
-# --------------
-#  set :ssh_options, {
-#    keys: %w(/home/rlisowski/.ssh/id_rsa),
-#    forward_agent: false,
-#    auth_methods: %w(password)
-#  }
-#
-# The server-based syntax can be used to override options:
-# ------------------------------------
-# server "example.com",
-#   user: "user_name",
-#   roles: %w{web app},
-#   ssh_options: {
-#     user: "user_name", # overrides user setting above
-#     keys: %w(/home/user_name/.ssh/id_rsa),
-#     forward_agent: false,
-#     auth_methods: %w(publickey password)
-#     # password: "please use keys"
-#   }
+set :stage, :production
+set :branch, ENV['REVISION'] || ENV['BRANCH_NAME'] || :master
+
+set :deploy_to, '/home/deploy/www'
+set :rvm_ruby_version, '2.4.0@westmarket'
+set :linked_files, %w(config/puma.rb)
+
+# Tasks
+# ======
+
+namespace :deploy do
+  desc 'Restart application'
+  task :restart do
+    on roles(:puma_role), in: :sequence, wait: 5 do
+      execute 'sudo restart puma-manager'
+    end
+  end
+
+  after  :finishing, :cleanup
+  after  :finishing, :restart
+end
