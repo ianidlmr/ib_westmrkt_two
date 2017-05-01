20d3dcf8a6c34902787c29de4bff8add578a91e9
WajidKagzi <wjkagzi@gmail.com>
2017-05-01 10:11:23 -0400

    Fix small issues with message and default skin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,13 @@
 # frozen_string_literal: true
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+  before_action :get_liked_unit_groups
 
   # TODO Remove before public launch
   if Rails.env.production?
     http_basic_authenticate_with name: 'railway', password: 'railwaydevbbq' # keeping the same credentials as integration server
   end
 
-  protect_from_forgery with: :exception
-  before_action :get_liked_unit_groups
 
   def get_liked_unit_groups
     if user_signed_in?
