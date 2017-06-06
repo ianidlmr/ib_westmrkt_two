01d9d7407ce5a5fa0733ef1fe403cd2693178c2f
ian bruce <ian@idlmr.com>
2017-06-06 11:10:38 -0400

    Another attempt to fix airbrake

--- config/initializers/airbrake.rb
+++ config/initializers/airbrake.rb
@@ -1,95 +1,61 @@
-# Every Airbrake notifier must configure
-# two options: `project_id` and `project_key`.
-Airbrake.configure do |c|
-  c.project_id = 145024
-  c.project_key = 'bb8427aeef79f963c2702085b183ef6d'
-end
 
-# Asynchronous error delivery.
-begin
-  1/0
-rescue ZeroDivisionError => ex
-  # Return value is always `nil`.
-  Airbrake.notify(ex)
-end
+ # frozen_string_literal: true
+ # Airbrake is an online tool that provides robust exception tracking in your Rails
+ # applications. In doing so, it allows you to easily review errors, tie an error
+ # to an individual piece of code, and trace the cause back to recent
+ # changes. Airbrake enables for easy categorization, searching, and prioritization
+ # of exceptions so that when errors occur, your team can quickly determine the
+ # root cause.
+ #
+ # Configuration details:
+ # https://github.com/airbrake/airbrake-ruby#configuration
 
-puts 'A ZeroDivisionError was sent to Airbrake asynchronously!',
-     "Find it at your project's dashboard on https://airbrake.io"
+ Airbrake.configure do |c|
+   # You must set both project_id & project_key. To find your project_id and
+   # project_key navigate to your project's General Settings and copy the values
+   # from the right sidebar.
+   # https://github.com/airbrake/airbrake-ruby#project_id--project_key
+   c.project_id = 145024 # 'true' for errbit projects
 
-# Synchronous error delivery.
-begin
-  1/0
-rescue ZeroDivisionError => ex
-  # Return value is a Hash.
-  response = Airbrake.notify_sync(ex)
-end
+  #  c.host = 'https://errbit.devbbq.com'
+   c.project_key = 'bb8427aeef79f963c2702085b183ef6d'
 
-puts "\nAnother ZeroDivisionError was sent to Airbrake, but this time synchronously.",
-     "See it at #{response['url']}"
+   # Configures the root directory of your project. Expects a String or a
+   # Pathname, which represents the path to your project. Providing this option
+   # helps us to filter out repetitive data from backtrace frames and link to
+   # GitHub files from our dashboard.
+   # https://github.com/airbrake/airbrake-ruby#root_directory
+   c.root_directory = Rails.root
 
+   # By default, Airbrake Ruby outputs to STDOUT. In Rails apps it makes sense to
+   # use the Rails' logger.
+   # https://github.com/airbrake/airbrake-ruby#logger
+   c.logger = Rails.logger
 
-# # Airbrake is an online tool that provides robust exception tracking in your Rails
-# # applications. In doing so, it allows you to easily review errors, tie an error
-# # to an individual piece of code, and trace the cause back to recent
-# # changes. Airbrake enables for easy categorization, searching, and prioritization
-# # of exceptions so that when errors occur, your team can quickly determine the
-# # root cause.
-# #
-# # Configuration details:
-# # https://github.com/airbrake/airbrake-ruby#configuration
-# Airbrake.configure do |c|
-#   # You must set both project_id & project_key. To find your project_id and
-#   # project_key navigate to your project's General Settings and copy the values
-#   # from the right sidebar.
-#   # https://github.com/airbrake/airbrake-ruby#project_id--project_key
-#   c.project_id = 145024
-#   c.project_key = 'bb8427aeef79f963c2702085b183ef6d'
-#
-#   # Configures the root directory of your project. Expects a String or a
-#   # Pathname, which represents the path to your project. Providing this option
-#   # helps us to filter out repetitive data from backtrace frames and link to
-#   # GitHub files from our dashboard.
-#   # https://github.com/airbrake/airbrake-ruby#root_directory
-#   c.root_directory = Rails.root
-#
-#   # By default, Airbrake Ruby outputs to STDOUT. In Rails apps it makes sense to
-#   # use the Rails' logger.
-#   # https://github.com/airbrake/airbrake-ruby#logger
-#   c.logger = Rails.logger
-#
-#   # Configures the environment the application is running in. Helps the Airbrake
-#   # dashboard to distinguish between exceptions occurring in different
-#   # environments. By default, it's not set.
-#   # NOTE: This option must be set in order to make the 'ignore_environments'
-#   # option work.
-#   # https://github.com/airbrake/airbrake-ruby#environment
-#   c.environment = Rails.env
-#
-#   # Setting this option allows Airbrake to filter exceptions occurring in
-#   # unwanted environments such as :test. By default, it is equal to an empty
-#   # Array, which means Airbrake Ruby sends exceptions occurring in all
-#   # environments.
-#   # NOTE: This option *does not* work if you don't set the 'environment' option.
-#   # https://github.com/airbrake/airbrake-ruby#ignore_environments
-#   c.ignore_environments = %w(test)
-#
-#
-#   # A list of parameters that should be filtered out of what is sent to
-#   # Airbrake. By default, all "password" attributes will have their contents
-#   # replaced.
-#   # https://github.com/airbrake/airbrake-ruby#blacklist_keys
-#   c.blacklist_keys = [/password/i]
-# end
-#
-# Airbrake.configure(:project_a) do |c|
-#   c.project_id = 145024
-#   c.project_key = 'bb8427aeef79f963c2702085b183ef6d'
-# end
-#
-# Airbrake[:project_a].notify('Oops!', params)
-# %i(project_a project_b).each { |notifier_name| Airbrake[notifier_name].close }
-#
-# # If Airbrake doesn't send any expected exceptions, we suggest to uncomment the
-# # line below. It might simplify debugging of background Airbrake workers, which
-# # can silently die.
-# # Thread.abort_on_exception = ['test', 'development'].include?(Rails.env)
+   # Configures the environment the application is running in. Helps the Airbrake
+   # dashboard to distinguish between exceptions occurring in different
+   # environments. By default, it's not set.
+   # https://github.com/airbrake/airbrake-ruby#environment
+   c.environment = Rails.env
+
+   # Setting this option allows Airbrake to filter exceptions occurring in
+   # unwanted environments such as :test. By default, it is equal to an empty
+   # Array, which means Airbrake Ruby sends exceptions occurring in all
+   # environments.
+   # https://github.com/airbrake/airbrake-ruby#ignore_environments
+   c.ignore_environments = %w(development test)
+ end
+
+ Airbrake.add_filter do |notice|
+   # The library supports nested exceptions, so one notice can carry several
+   # exceptions.
+
+   ignored_exceptions = [
+     'ActiveRecord::RecordNotFound',
+     'ActionController::InvalidAuthenticityToken'
+   ]
+
+   if notice[:errors].any? { |error| ignored_exceptions.include? error[:type] }
+     notice.ignore!
+   end
+ end
