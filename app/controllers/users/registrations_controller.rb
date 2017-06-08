2db4b0bcda4812a70592d51f41873009a39e8305
ian bruce <ian@idlmr.com>
2017-06-08 15:21:06 -0400

    Added another skip before action

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -3,6 +3,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
   before_action :configure_sign_up_params, only: [:create]
   before_action :configure_account_update_params, only: [:update]
   skip_before_action :verify_authenticity_token
+  skip_before_action :proceed_with_authorization
   
   respond_to :json
 
